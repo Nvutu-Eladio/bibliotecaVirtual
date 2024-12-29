e09d96ab17baad401e5ab5f10db9d47a0582e1bf
Nvutu-Eladio <Eladio2019@hotmail.com>
2024-12-29 15:53:35 -0300

    feat: documentando aplicaçcao

--- src/main/java/com/bibliotecaVirtual/api/controller/LivroController.java
+++ src/main/java/com/bibliotecaVirtual/api/controller/LivroController.java
@@ -4,6 +4,8 @@ import com.bibliotecaVirtual.api.dto.request.LivroRequestDTO;
 import com.bibliotecaVirtual.api.dto.response.LivroResponseDTO;
 import com.bibliotecaVirtual.api.repository.LivroRepository;
 import com.bibliotecaVirtual.api.service.LivroService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,6 +30,10 @@ public class LivroController {
     @Autowired
     private LivroRepository livroRepository;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "409", description = "Livro já cadastrado no banco de dados")
+    })
     @PostMapping
     public ResponseEntity<LivroResponseDTO> criar(@RequestBody LivroRequestDTO requestDTO) {
 
@@ -39,18 +45,28 @@ public class LivroController {
                 .toUri()).body(response);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista todos os livros"),
+    })
     @GetMapping
     public ResponseEntity<List<LivroResponseDTO>> obterLivro() {
         return ResponseEntity.ok(livroService.obterLivro());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atualiza um determinado livro"),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar livro no banco de dados")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<LivroResponseDTO> atualizar(@PathVariable("id") String id, @RequestBody LivroRequestDTO requestDTO) {
         LivroResponseDTO responseDTO = livroService.atualizar(id, requestDTO);
         return ResponseEntity.ok().body(responseDTO);
     }
 
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", description = "Livro não encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<LivroResponseDTO> apagar(@PathVariable("id") String id) {
         this.livroService.apagar(id);
